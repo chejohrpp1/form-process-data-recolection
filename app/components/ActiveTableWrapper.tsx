0fb2df388fda9d1eaf80bdbe11829b55fb742d9f
chejohrpp1 <chejohrpp@gmail.com>
2025-03-11 16:49:08 -0600

    improve the design

--- app/components/ActiveTableWrapper.tsx
+++ app/components/ActiveTableWrapper.tsx
@@ -1,9 +1,8 @@
-// components/ActiveTableWrapper.tsx
 "use client";
 
 import dynamic from "next/dynamic";
 
-// Carga ActiveTable de manera dinámica sin SSR
+// Carga ActiveTable dinámicamente sin SSR
 const ActiveTable = dynamic(
   () => import("active-table-react").then((mod) => mod.ActiveTable),
   { ssr: false }
@@ -17,8 +16,63 @@ interface TableProps {
 export default function ActiveTableWrapper({ data }: TableProps) {
   return (
     <ActiveTable
-      data={data}
+      maxColumns={5}
+      dragColumns={false}
+      customColumnTypes={[
+        {
+          name: "process_type",
+          label: {
+            options: [
+              { text: "Vital", backgroundColor: "#4ddd3878" },
+              { text: "Soporte", backgroundColor: "#ff606082" },
+              { text: "otro", backgroundColor: "#ff940f8f" },
+            ],
+            canAddMoreOptions: false,
+          },
+          iconSettings: {
+            reusableIconName: "Select",
+          },
+        },
+        {
+          name: "periodicity",
+          select: {
+            options: ["Diario", "Semanal", "Quincenal", "Mensual", "Trimestral", "Semestral", "Anual"],
+            canAddMoreOptions: true
+          },
+          iconSettings : {
+            reusableIconName: "Select"
+          }
+        }
+      ]}
+      customColumnsSettings={[
+        {
+          headerName: "Tipo de proceso",
+          isHeaderTextEditable: false,
+          defaultColumnTypeName: "process_type",
+        },
+        {
+          headerName: "Nombre del proceso",
+          defaultColumnTypeName: "string",
+          isHeaderTextEditable: false,
+        },
+        {
+          headerName: "Descripción del proceso",
+          defaultColumnTypeName: "string",
+          isHeaderTextEditable: false,
+        },
+        {
+          headerName: "Periodicidad",
+          defaultColumnTypeName: "periodicity",
+          isHeaderTextEditable: false,
+        },
+        {
+          headerName: "Áreas involucradas",
+          defaultColumnTypeName: "string",
+          isHeaderTextEditable: false,
+        },
+      ]}
       headerStyles={{ default: { backgroundColor: "#d6d6d630" } }}
+      data={data}
     />
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
 import { FormManager } from "./components/FormManager";
 
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-between min-h-screen p-8 sm:p-20 text-black bg-gradient-to-r from-rose-100 to-teal-100 space-y-8">
       <h1
-        className="mb-6 border-y text-5xl font-bold [border-image:linear-gradient(to_right,transparent,theme(colors.slate.300/.8),transparent)1] md:text-6xl"
+        className="mb-6 text-center border-y text-5xl font-bold 
+            [border-image:linear-gradient(to_right,transparent,theme(colors.slate.300/.8),transparent)1] 
+            md:text-6xl"
         data-aos="zoom-y-out"
         data-aos-delay={150}
       >
-        Formulario de Procesos
+        Gestión y Documentación de Procesos del Fondo de Garantía
       </h1>
-      <p className="mb-8 text-lg text-gray-700">Algo de Descripcion</p>
-      <FormManager/>
+
+      <p className="mb-8 text-lg text-gray-700 text-center px-6 md:px-20">
+        Esta herramienta facilita la documentación y gestión de procesos dentro
+        de la organización. Permite registrar y clasificar los procesos según su
+        importancia, periodicidad y áreas involucradas, asegurando una mejor
+        organización y control de las actividades clave.
+      </p>
+      <FormManager />
     </div>
   );
 }
